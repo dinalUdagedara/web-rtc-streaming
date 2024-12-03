37e1cc1be4ddd7f79ce047880e2eb444e4c55ecc
Thiyunu <dinal.bandara@gmail.com>
2024-12-03 12:22:48 +0530

    feat: used web rtc for recording

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import Recorder from "@/components/recorder";
+"use client";
+import dynamic from "next/dynamic";
+const Recorder = dynamic(() => import("@/components/recorder"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
--- components/recorder.tsx
+++ components/recorder.tsx
@@ -1,36 +1,62 @@
 "use client";
 import React, { useState, useRef } from "react";
+import RecordRTC from "recordrtc";
+import { FaMicrophone, FaStop } from "react-icons/fa";
 
 const Recorder: React.FC = () => {
   const [isRecording, setIsRecording] = useState(false);
   const [audioUrl, setAudioUrl] = useState<string | null>(null);
-  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
-  const audioChunksRef = useRef<Blob[]>([]);
+  const recorderRef = useRef<RecordRTC | null>(null);
+  const mediaStreamRef = useRef<MediaStream | null>(null);
 
   const startRecording = async () => {
     try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+      mediaStreamRef.current = stream;
 
+      setTimeout(() => {
+        // const recorder = new RecordRTC(stream, {
+        //   type: "audio",
+        //   recorderType: RecordRTC.StereoAudioRecorder,
+        //   mimeType: "audio/wav",
+        //   timeSlice: 500, // Increased timeSlice to ensure initial audio is captured
+        //   sampleRate: 46000,
+        //   numberOfAudioChannels: 1,
+        //   // ondataavailable: (blob: Blob) => {
+        //   //   console.log("ondataavailable", blob);
+        //   //   const reader = new FileReader();
+        //   //   reader.onloadend = () => {
+        //   //     const base64data = reader.result;
+        //   //     socket?.emit("stream_audio", {
+        //   //       web_socket_id: id,
+        //   //       session_id: chatSessionId,
+        //   //       message_id: messageId,
+        //   //       user_id: userId,
+        //   //       audio_data: base64data,
+        //   //       mime_type: "audio/wav",
+        //   //     });
+        //   //   };
+        //   //   reader.readAsDataURL(blob);
+        //   // },
+        // });
 
-      
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const mediaRecorder = new MediaRecorder(stream);
-      mediaRecorderRef.current = mediaRecorder;
-
-      mediaRecorder.ondataavailable = (event) => {
-        console.log("Media ", audioChunksRef);
-        audioChunksRef.current.push(event.data);
-      };
-
-      mediaRecorder.onstop = () => {
-        const audioBlob = new Blob(audioChunksRef.current, {
-          type: "audio/wav",
+        let recorder = new RecordRTC(stream, {
+          type: "audio",
+          recorderType: RecordRTC.StereoAudioRecorder,
+          mimeType: "audio/wav",
+          timeSlice: 500,
+          sampleRate: 46000,
+          numberOfAudioChannels: 1,
+          ondataavailable: (blob: Blob) => {
+            console.log("ondataavailable", blob);
+          },
         });
-        const audioUrl = URL.createObjectURL(audioBlob);
-        setAudioUrl(audioUrl);
-        audioChunksRef.current = []; // Clear chunks for the next recording
-      };
+        recorderRef.current = recorder;
+        recorder.startRecording();
+      }, 500);
 
-      mediaRecorder.start();
       setIsRecording(true);
     } catch (err) {
       console.error("Error accessing microphone: ", err);
@@ -38,27 +64,56 @@ const Recorder: React.FC = () => {
   };
 
   const stopRecording = () => {
-    if (mediaRecorderRef.current) {
-      mediaRecorderRef.current.stop();
-      setIsRecording(false);
+    if (recorderRef.current) {
+      // Add a buffer before stopping the recording
+      setTimeout(() => {
+        // recorderRef.current?.stopRecording(() => {
+        //   const socket = webSocketService.getSocket();
+
+        //   const userId = localStorage.getItem("userId");
+
+        //   const stopAudioRequest = {
+        //     web_socket_id: websocketId,
+        //     session_id: chatSessionId,
+        //     message_id: messageId,
+        //     user_id: userId,
+        //   };
+        //   socket?.emit("stop_audio", stopAudioRequest);
+
+        //   // const blob = recorderRef.current?.getBlob();
+        //   // if (blob) {
+        //   //   const audioUrl = URL.createObjectURL(blob); // Newly added line
+        //   //   setAudioUrl(audioUrl); // Newly added line
+        //   //   const audio = new Audio(audioUrl);
+        //   //   audio.play();
+        //   // }
+        // });
+
+        setIsRecording(false);
+
+        if (mediaStreamRef.current) {
+          mediaStreamRef.current.getTracks().forEach((track) => track.stop());
+          mediaStreamRef.current = null;
+        }
+      }, 1000); // 500ms buffer
     }
   };
 
   return (
-    <div className="bg-gray-400 flex justify-center flex-col text-center items-center h-96 w-96 rounded-xl p-4">
+    <div className=" bg-gray-700 text-white  flex justify-center flex-col text-center items-center h-[550px] w-[350px] rounded-xl p-4">
       {!isRecording ? (
         <button
           onClick={startRecording}
-          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300"
+          className="bg-green-500 text-white px-4 py-4  hover:bg-green-600 transition duration-300 rounded-full"
         >
-          Start Recording
+          <FaMicrophone className="h-10 w-10" />
         </button>
       ) : (
         <button
           onClick={stopRecording}
-          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300"
+          className="bg-red-500 text-white px-4 py-4  rounded hover:bg-red-600 transition duration-300"
         >
-          Stop Recording
+          <FaStop className="h-10 w-10" />
         </button>
       )}
       {audioUrl && (
